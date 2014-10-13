e7507d3e64c9f7632bedc05b2ebfdc36dec384ab
Marco <Marco@marcos-imac.fritz.box>
2014-10-13 13:17:12 +0200

    Halbfertig, Reset fehlt, event. Vibration und Foto Capture

--- redButton/ViewController.swift
+++ redButton/ViewController.swift
@@ -7,19 +7,105 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    
+    
+    var currentIndex = 0
+    var audioPlayer: AVAudioPlayer?
+    
+    
+    
+    @IBOutlet weak var dontPressTheRedButtonLabel: UILabel!
+    
+    @IBOutlet weak var ImageViewBang: UIImageView!
 
+    @IBOutlet weak var test: UIButton!
+    
+    
+    
+   
+    
+    
+    
+   
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+
+        
+        
+      
+}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+        @IBAction func redButtonPressed(sender: UIButton) {
+    
+        
+        var anzahlShouts = shouts.count
+        
+        let shoutOut = shouts[currentIndex]
+        
+        
+        if currentIndex < anzahlShouts {
+            
+            currentIndex += 1
+        
+            dontPressTheRedButtonLabel.text = ("\(shoutOut)")
+        
+     //    if currentIndex >= anzahlShouts {
+            
+     //       currentIndex = 0
+            
+      //     }
+        
+            if currentIndex >= anzahlShouts   {
+            
+                // Entfernt den UIButton "redButtonPressed" mit dem Parameter "sender"
+                
+                [sender.removeFromSuperview()]
+                
+                ImageViewBang.hidden = false
+                
+                                
+               
+                
+                // audioPlayer wird oben deffiniert
+                
+                if let path = NSBundle.mainBundle().pathForResource("Byeball", ofType: "wav") {
+                    audioPlayer = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: path), fileTypeHint: "wav", error: nil)
+                    
+                    if let sound = audioPlayer {
+                        
+                        sound.prepareToPlay()
+                        
+                        sound.play()
+                }
+            }
+
+        }
+        
+        
+        
+        
+    }
+        
+        
+        
+        
+    }
+    
+
+   
 
 }
 
---
+++ redButton/sprueche.swift
@@ -0,0 +1,13 @@
+//
+//  sprueche.swift
+//  redButton
+//
+//  Created by Marco F.A. Corzani on 12.10.14.
+//  Copyright (c) 2014 Alphaweb. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+var shouts = ["Dont press it once more!", "Dont do that again!", "I beg you, dont do it again!", ""]
